7c0884d21d74fea14678254f894f75a39202c797
Michael <ninjacksnake@hotmail.com>
2024-08-20 01:09:26 -0400

    Chnage the dbto aws

--- monorepo/agro-ninja-api/src/utils/services/db_services.js
+++ monorepo/agro-ninja-api/src/utils/services/db_services.js
@@ -6,19 +6,19 @@ const host = process.env.HOST;
 const database = process.env.DATABASE;
 const username = process.env.DBUSERNAME;
 const password = process.env.DBPASSWORD;
-const port = process.env.PORT;
+const port = process.env.PORT || 5430;
 
 const sequelize = new Sequelize(database, username, password, {
   host: host,
   dialect: dialect,
   getmasterpublicker: 1,
   port: port,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true, // Ensure SSL is used
-  //     rejectUnauthorized: false, // Accept self-signed certs
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true, // Ensure SSL is used
+      rejectUnauthorized: false, // Accept self-signed certs
+    },
+  },
   logging: console.log,
 });
 

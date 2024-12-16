9b786b4109ed6c2b6106039ceabec83eeaad5d07
Michael Fermin <ninjacksnake@gmail.com>
2024-12-16 11:14:14 -0400

    moving to local environment, removing imagekit, changing environment variables

--- monorepo/agro-ninja-api/src/app.js
+++ monorepo/agro-ninja-api/src/app.js
@@ -5,20 +5,13 @@ const port = 3004;
 const router = require("./routers/appRouter.js");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const Imagekit = require("imagekit");
 
-const imagekit = new Imagekit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-});
 
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(cors());
 app.use(router);
 
 app.get("/auth", (req, res) => {
-  const authenticationParameters = imagekit.getAuthenticationParameters();
   res.send(authenticationParameters);
 });
 
